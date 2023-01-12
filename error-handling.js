7f0319c804027be168628854b102a53358f0ad28
Kyle Seeley <kyleseeley@Kyles-MacBook-Air-2.local>
2023-01-11 17:00:53 -0700

    first two complete

--- error-handling.js
+++ error-handling.js
@@ -1,20 +1,42 @@
 // 1.
-function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-}
+// function sum(array) {
+//   let sum = 0;
+//   if(array === null){
+//     throw new Error("Array is null")
+//   }
+//   for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+//   }
+//   return sum;
+// }
 
-let res = sum(null);
-console.log(res);
+// let res = sum(null);
+// try{
+//   console.log(res);
+// } catch(error){
+//   console.log(`${error.name}: ${error.message}`)
+// }
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
+// sayName("Alex");
+// sayName(1);
 // Your code here
+const sayName = name => {
+  if(typeof name !== 'string'){
+    throw new Error("Invalid name! Must be a string!")
+  } else {
+    return name;
+  }
+}
+//sayName("Alex");
+//sayName(1);
+try{
+  console.log(sayName("Alex"))
+  console.log(sayName(1))
+} catch(error) {
+  console.log(`${error.name}: ${error.message}`)
+}
 
 // 3.
 function greet(greeting) {
